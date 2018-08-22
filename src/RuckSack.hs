bbea524617d036474bcaa2c0e98f1225cace55b1
Erik Stevenson <eriknstevenson@gmail.com>
2018-08-22 13:33:00 -0400

    Make size work in constant time

--- src/RuckSack.hs
+++ src/RuckSack.hs
@@ -6,24 +6,25 @@ module RuckSack
     , size
     ) where
 
-newtype RuckSack a = RuckSack [a] deriving (Eq, Show)
+data RuckSack a = RuckSack Int [a] deriving (Eq, Show)
 
 instance Functor RuckSack where
-    fmap f (RuckSack xs) = RuckSack $ fmap f xs
+    fmap f (RuckSack count xs) = RuckSack count $ fmap f xs
 
 instance Monoid (RuckSack a) where
-    (RuckSack a) `mappend` (RuckSack b) = RuckSack $ a `mappend` b
+    (RuckSack aLength a) `mappend` (RuckSack bLength b) =
+        RuckSack (aLength + bLength) (a `mappend` b)
     mempty = mkRuckSack
 
 mkRuckSack :: RuckSack a
-mkRuckSack = RuckSack []
+mkRuckSack = RuckSack 0 []
 
 add :: a ->  RuckSack a -> RuckSack a
-add x (RuckSack xs) = RuckSack (x:xs)
+add x (RuckSack count xs) = RuckSack (count + 1) (x:xs)
 
 isEmpty :: RuckSack a -> Bool
-isEmpty (RuckSack []) = True
+isEmpty (RuckSack 0 _) = True
 isEmpty _ = False
 
 size :: RuckSack a -> Int
-size (RuckSack xs) = length xs
+size (RuckSack count _) = count
